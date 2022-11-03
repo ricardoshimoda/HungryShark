612b1ab982f2b8a5f48d5dbdc5eacb1a725e7e72
Ricardo Shimoda Nakasako <rshimoda@gmail.com>
2022-11-03 14:31:52 -0400

    Moves around the player

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    static private Movement _S;
+    static public Movement S {
+        get {
+            return _S;
+        }
+        private set {
+            if (_S != null) {
+                Debug.LogWarning("Second attempt to set Movement singleton _S");
+            }
+            _S= value;
+        }
     }
 
-    // Update is called once per frame
+    [Tooltip("Constant speed for the shark")]
+    public float speed = 10;
+
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 movement = Vector3.ClampMagnitude(new Vector3(x, y, 0), 1.0f);
+        _rb.velocity = movement * speed;
     }
 }
